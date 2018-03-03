90943953e14206109affe32feac80e459a7cdae1
Matilde W. Enevoldsen <matildeene@outlook.com>
2018-03-03 19:42:33 +0100

    Folder & File structure setup, as well resetting and base button style

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <p>Hi How's u?</p>
+                <button className="btn">Hi</button>
+            </div>
+        );
+    }
+}
+
+export default App;
